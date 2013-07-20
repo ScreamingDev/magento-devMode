cdfbe845af322c7038b7d6be4866a56dc2853d5d
Mike Pretzlaw <pretzlaw@gmail.com>
2013-07-20 23:11:32 +0200

    Added menu with first entry

---
+++ app/code/local/LeMike/DevMode/controllers/Adminhtml/DeveloperController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    DeveloperController.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+/**
+ * Class DeveloperController.
+ *
+ * @category   mage_devMail
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Adminhtml_DeveloperController extends Mage_Adminhtml_Controller_Action
+{
+    public function aboutAction()
+    {
+        $this->loadLayout()
+        ->_setActiveMenu('lemike_devmode/about')
+        ->_title($this->__('About Developer Mode'));
+
+        $this->renderLayout();
+    }
+}
