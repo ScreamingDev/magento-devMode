12733b64cc9db75c226098a9ddace7f4e4dff90f
Mike Pretzlaw <pretzlaw@gmail.com>
2013-07-21 22:58:17 +0200

    #5 Delete all orders
    - Added Menu Entry and ACL for Sales
    - Added Tab for Orders
    - Added templates for Tab
    - Added JS for Sales
    - Added Controller for Sales and layout generating tabs
    - Added Controller for Sales_Order and action ::deleteAll to delete all orders

---
+++ app/code/community/LeMike/DevMode/Block/Sales/Tabs.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * LeMike_DevMode Catalog page left menu
+ *
+ * @category   LeMike_DevMode
+ * @package    LeMike_DevMode_Block_Sales
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ */
+class LeMike_DevMode_Block_Sales_Tabs extends Mage_Adminhtml_Block_Widget_Tabs
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('page_tabs');
+        $this->setDestElementId('devmode_sales');
+        $this->setTitle(Mage::helper('lemike_devmode')->__('Sales Tools'));
+    }
+
+
+    protected function _beforeToHtml()
+    {
+        /** @var Mage_Adminhtml_Block_Template $ordersBlock */
+        $ordersBlock = $this->getLayout()->createBlock('adminhtml/template', 'sales.orders');
+        $ordersBlock->setTemplate('lemike/devmode/sales/orders.phtml');
+
+        $this->addTab(
+            'main_section',
+            array(
+                 'label'   => Mage::helper('lemike_devmode')->__('Products'),
+                 'title'   => Mage::helper('lemike_devmode')->__('Products'),
+                 'content' => $ordersBlock->toHtml(),
+                 'active'  => true
+            )
+        );
+
+        return parent::_beforeToHtml();
+    }
+}
---
+++ app/code/community/LeMike/DevMode/controllers/Adminhtml/Developer/SalesController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    DeveloperController.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+/**
+ * Class DeveloperController.
+ *
+ * @category   mage_devMail
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Adminhtml_Developer_SalesController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $helper = Mage::helper('lemike_devmode');
+
+        $this->loadLayout()
+        ->_setActiveMenu('lemike_devmode/sales');
+
+        $this->_title($helper->__('Development'))
+        ->_title($helper->__('Sales'));
+
+        $this->renderLayout();
+    }
+}
