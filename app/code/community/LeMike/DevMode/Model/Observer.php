12de6ac6766a892451f6b772acfb0f92273dc48c
Mike Pretzlaw <pretzlaw@gmail.com>
2013-08-22 23:16:22 +0200

    #15 Show used events with their observers

---
+++ app/code/community/LeMike/DevMode/Model/Observer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    Observer.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+/**
+ * Class Observer.
+ *
+ * @category   mage_devMail
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Model_Observer extends Mage_Core_Model_Abstract
+{
+    public function controllerFrontSendResponseBefore($observer)
+    {
+        /** @var Mage_Core_Controller_Varien_Front $front */
+        $front = $observer->getFront();
+
+        if ($front->getRequest()->has('__events'))
+        {
+            $reflectApp        = new ReflectionObject(Mage::app());
+            $reflectEventCache = $reflectApp->getProperty('_events');
+            $reflectEventCache->setAccessible(true);
+
+            $value = $reflectEventCache->getValue(Mage::app());
+
+            $returnSet = array();
+            foreach ($value as $area => $eventSet)
+            {
+                $returnSet = array_merge($returnSet, array_keys($eventSet));
+            }
+
+            $front->getResponse()->setBody('<html><body><pre>' . print_r($value, true) . '</pre></body></html>');
+        }
+    }
+} 
