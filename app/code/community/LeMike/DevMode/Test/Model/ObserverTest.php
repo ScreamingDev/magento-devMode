c5121488a7f937e84bd3790e9e8e6cb8d3793636
Mike Pretzlaw <pretzlaw@gmail.com>
2013-08-24 12:06:59 +0200

    #15 Test Observer on ?__events=1

---
+++ app/code/community/LeMike/DevMode/Test/Model/ObserverTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    ObserverTest.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+class LeMike_DevMode_Model_ObserverTest extends EcomDev_PHPUnit_Test_Case_Controller
+{
+    public function testEventsQuery()
+    {
+        /** @var EcomDev_PHPUnit_Controller_Request_Http $request */
+        $request = $this->getRequest();
+        $request->setParam('__events', 1);
+
+        $this->dispatch('customer/account/login');
+        $this->assertResponseBodyContains('<pre>');
+        $this->assertResponseBodyContains('global');
+        $this->assertResponseBodyContains('Array');
+
+        $request->setParam('__events', null);
+    }
+}
