d2d93dc21d53eea45555abd3998d817db5c53167
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-07 09:47:16 +0200

    fix #26 added scripts to delete categories and products

---
+++ shell/deleteCategories.php
@@ -0,0 +1,65 @@
+<?php
+
+include 'abstract.php';
+
+class LeMike_DevMode_Shell_DeleteCategories extends Mage_Shell_Abstract
+{
+
+    /**
+     * Run script
+     *
+     */
+    public function run()
+    {
+        print_r($this->execute());
+    }
+
+
+    public function execute()
+    {
+        $deleteAll = array(
+            'amount'    => 0,
+            'processed' => 0,
+            'errors'    => array(),
+        );
+
+        $collection          = $this->_getProductSet();
+        $deleteAll['amount'] = $collection->count();
+
+        foreach ($collection as $item)
+        {
+            $item = $item->load($item->getId());
+            /** @var Mage_Catalog_Model_Product $item */
+            $item->delete();
+            $deleteAll['processed']++;
+
+            // free some memory
+            unset($item);
+        }
+
+        return $deleteAll;
+    }
+
+
+    /**
+     * .
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    protected function _getProductSet()
+    {
+        // Set store defaults for Magento
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        /* @var $productModel Mage_Catalog_Model_Product */
+        $model = Mage::getModel('catalog/category');
+
+        /* @var $collection Mage_Catalog_Model_Resource_Product_Collection */
+        $collection = $model->getCollection();
+
+        return $collection;
+    }
+}
+
+$cmd = new LeMike_DevMode_Shell_DeleteCategories();
+$cmd->run();
---
+++ shell/deleteProducts.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'abstract.php';
+
+class LeMike_DevMode_Shell_DeleteProducts extends Mage_Shell_Abstract
+{
+
+    /**
+     * Run script
+     *
+     */
+    public function run()
+    {
+        print_r($this->execute());
+    }
+
+
+    public function execute()
+    {
+        $deleteAll = array(
+            'amount'    => 0,
+            'processed' => 0,
+            'errors'    => array(),
+        );
+
+        $productSet          = $this->_getProductSet();
+        $deleteAll['amount'] = $productSet->count();
+
+        foreach ($productSet as $product)
+        {
+            $productId = $product->getId();
+            $product   = $product->load($productId);
+            /** @var Mage_Catalog_Model_Product $product */
+            $product->delete();
+            $deleteAll['processed']++;
+            echo "Deleted $productId ({$deleteAll['processed']}/{$deleteAll['amount']})" . PHP_EOL;
+        }
+
+        return $deleteAll;
+    }
+
+
+    /**
+     * .
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    protected function _getProductSet()
+    {
+        // Set store defaults for Magento
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        /* @var $productModel Mage_Catalog_Model_Product */
+        $productModel = Mage::getModel('catalog/product');
+
+        /* @var $productSet Mage_Catalog_Model_Resource_Product_Collection */
+        $productSet = $productModel->getCollection();
+
+        return $productSet;
+    }
+}
+
+$cmd = new LeMike_DevMode_Shell_DeleteProducts();
+$cmd->run();
