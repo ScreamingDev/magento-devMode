fcefdc57b1e19c67028a63f1a7c9cf66f28f379f
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-10 23:09:20 +0200

    #30 Write correct unit test for LeMike_DevMode_Model_Core_Email_Template::send
    - Make a fixture with the option lemike_devmode_core/email/active disabled (0)
    - Test/Assert that fixture
    - Mock the Mail-Template so that it only outputs a unique string (in the usual way with $foo->getMock() etc.)
    - Test/Assert that mock / mocking the according method
    - Dispatch newsletter/subscriber/new with the post ['email' => 'lemike_devmode']
    - Test if / Assert that the dispatch was successful
    - Fetch the output
    - Test against / Assert the previous defined unique string
    - check if magento output is supressed by testing helper and current response body

---
+++ app/code/community/LeMike/DevMode/Test/Model/Core/Email/TemplateTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    TemplateTest.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      0.3.0
+ */
+
+class LeMike_DevMode_Test_Model_Core_Email_TemplateTest extends EcomDev_PHPUnit_Test_Case_Controller
+{
+    /**
+     * Test newsletter direct output.
+     *
+     * @loadFixture core_mail_disabled
+     * @return void
+     */
+    public function testRegisterNewsletter_EmailDisabled()
+    {
+        /*
+         * }}} precondition {{{
+         */
+        $this->assertEquals('0', Mage::getStoreConfig('lemike_devmode_core/email/active'));
+        $this->assertFalse(Mage::helper('lemike_devmode/config')->isMailAllowed());
+
+        $templateText = md5(uniqid());
+        $coreEmailTemplateMock = $this->getModelMock('core/email_template', array('getTemplateText'));
+        $coreEmailTemplateMock->expects($this->any())->method('getTemplateText')->will(
+            $this->returnValue($templateText)
+        );
+        $this->replaceByMock('model', 'core/email_template', $coreEmailTemplateMock);
+
+        $this->assertEquals($templateText, Mage::getModel('core/email_template')->getTemplateText());
+
+        /*
+         * }}} main condition {{{
+         */
+        ob_start();
+        $this->getRequest()->setMethod('POST');
+        $this->getRequest()->setPost('email', 'lemike_devmode' . uniqid() . '@example.org');
+        $this->dispatch('newsletter/subscriber/new');
+
+        $this->assertRequestRoute('newsletter/subscriber/new');
+
+        $contents = ob_get_clean();
+        $this->assertSame($templateText, $contents);
+
+        /*
+         * }}} postcondition {{{
+         */
+        $this->assertTrue(Mage::helper('lemike_devmode')->disableMagentoDispatch());
+        $this->assertEmpty($this->getResponse()->getOutputBody());
+        $this->assertEmpty($this->getResponse()->getBody());
+    }
+}
