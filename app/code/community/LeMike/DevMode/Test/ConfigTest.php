a5baea7a71ad993491efb0c70b0da659321fd901
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-11 21:51:37 +0200

    Test on config.xml

--- app/code/community/LeMike/DevMode/Test/ConfigTest.php
+++ app/code/community/LeMike/DevMode/Test/ConfigTest.php
@@ -24,14 +24,85 @@
  * @copyright  2013 Mike Pretzlaw
  * @license    http://github.com/sourcerer-mike/mage_devMode/blob/master/LICENSE.md BSD 3-Clause ("BSD New")
  * @link       http://github.com/sourcerer-mike/mage_devMode
- * @since      0.2.0
+ * @since      0.3.0
  */
 class LeMike_DevMode_Test_ConfigTest extends EcomDev_PHPUnit_Test_Case_Config
 {
+    public function testEvents()
+    {
+        $observerAlias = 'lemike_devmode/observer';
+        $observer      = Mage::getModel($observerAlias);
+
+        /*
+         * }}} global {{{
+         */
+        $method = 'controllerFrontInitBefore';
+        $this->assertEventObserverDefined(
+            'global',
+            'controller_front_init_before',
+            $observerAlias,
+            $method
+        );
+        $this->assertTrue(method_exists($observer, $method));
+
+        $method = 'controllerActionPostdispatch';
+        $this->assertEventObserverDefined(
+            'frontend',
+            'controller_action_postdispatch',
+            $observerAlias,
+            $method
+        );
+        $this->assertTrue(method_exists($observer, $method));
+
+        $method = 'controllerActionPredispatchCustomerAccountLoginPost';
+        $this->assertEventObserverDefined(
+            'frontend',
+            'controller_action_predispatch_customer_account_loginPost',
+            $observerAlias,
+            $method
+        );
+        $this->assertTrue(method_exists($observer, $method));
+
+        $method = 'controllerFrontSendResponseBefore';
+        $this->assertEventObserverDefined(
+            'frontend',
+            'controller_front_send_response_before',
+            $observerAlias,
+            $method
+        );
+        $this->assertTrue(method_exists($observer, $method));
+    }
+
+
+    public function testGlobal()
+    {
+        $this->assertBlockAlias('lemike_devmode/template', 'LeMike_DevMode_Block_Template');
+        $this->assertHelperAlias('lemike_devmode', 'LeMike_DevMode_Helper_Data');
+        $this->assertModelAlias('lemike_devmode/config', 'LeMike_DevMode_Model_Config');
+    }
+
+
     public function testGlobalModelCoreRewrite()
     {
         $this->assertModelAlias('core/email', 'LeMike_DevMode_Model_Core_Email');
         $this->assertModelAlias('core/email_template', 'LeMike_DevMode_Model_Core_Email_Template');
         $this->assertModelAlias('core/email_transport', 'LeMike_DevMode_Model_Core_Email_Transport');
+    }
+
+
+    public function testModule()
+    {
+        $this->assertModuleIsActive();
+        $this->assertModuleVersion('0.3.0');
+    }
+
+
+    public function testAdmin()
+    {
+        $this->assertRouteFrontName('lemike_devmode', 'devmode', EcomDev_PHPUnit_Model_App::AREA_ADMIN);
+
+        $filename = 'LeMike_DevMode.xml';
+        $this->assertLayoutFileDefined('adminhtml', $filename);
+        $this->assertLayoutFileExistsInTheme('adminhtml', $filename, 'default');
     }
 }
