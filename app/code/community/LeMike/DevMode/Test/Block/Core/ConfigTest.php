608aac828ed9051731e9eb0e8576654ba24dff4f
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-15 11:38:55 +0200

    Additional tests on blocks

---
+++ app/code/community/LeMike/DevMode/Test/Block/Core/ConfigTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    ResourceTest.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+/**
+ * Class ResourceTest.
+ *
+ * @category   mage_devMail
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Test_Block_Core_ConfigTest extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * Tests GetModuleSet.
+     *
+     * @return null
+     */
+    public function testGetModel()
+    {
+        /*
+         * }}} preconditions {{{
+         */
+
+        /** @var LeMike_DevMode_Block_Core_Config $block */
+        $block = $this->getBlock('lemike_devmode/core_config');
+
+        $this->assertInstanceOf('LeMike_DevMode_Block_Core_Config', $block);
+
+        /*
+         * }}} main {{{
+         */
+
+        $model = $block->getModel();
+
+        $this->assertInstanceOf('Mage_Core_Model_Abstract', $model);
+        $this->assertInstanceOf('LeMike_DevMode_Model_Core_Config', $model);
+
+        /*
+         * }}} postcondition {{{
+         */
+
+        return null;
+    }
+}
