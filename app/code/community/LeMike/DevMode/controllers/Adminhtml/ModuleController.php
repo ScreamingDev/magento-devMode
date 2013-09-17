9a13d36e4529bd2d249afd2101901888c94485d9
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-17 20:04:49 +0200

    #46 menu entry for module editor with controller

---
+++ app/code/community/LeMike/DevMode/controllers/Adminhtml/ModuleController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devMail
+ * @package    ModulesController.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+
+/**
+ * Class ModulesController.
+ *
+ * @category   mage_devMail
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devMail/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devMail
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Adminhtml_ModulesController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+} 
