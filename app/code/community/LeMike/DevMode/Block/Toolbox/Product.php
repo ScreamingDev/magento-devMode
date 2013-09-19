c9a5fac4b0b41647dc482db8f55dfba2005d4ccd
Mike Pretzlaw <pretzlaw@gmail.com>
2013-09-19 21:50:30 +0200

    #49 link to backend for products

---
+++ app/code/community/LeMike/DevMode/Block/Toolbox/Product.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devmode
+ * @package    Cms.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devmode/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devmode
+ * @since      $VERSION$
+ */
+
+/**
+ * Class Cms.
+ *
+ * @category   mage_devmode
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devmode/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devmode
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Block_Toolbox_Product extends LeMike_DevMode_Block_Toolbox
+{
+    protected $_template = 'lemike/devmode/toolbox/product.phtml';
+
+
+    public function getEditUrl()
+    {
+        return $this->getBackendUrl(
+            'adminhtml/catalog_product/edit',
+            array('id' => Mage::registry('current_product')->getId())
+        );
+    }
+} 
