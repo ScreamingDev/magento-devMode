20dfb3ea5b15ba0c13a98aa41d2bf4cbaf663dc7
Mike Pretzlaw <pretzlaw@gmail.com>
2013-10-03 17:35:09 +0200

    link to backend for categories

---
+++ app/code/community/LeMike/DevMode/Block/Toolbox/Catalog/Category.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Contains class.
+ *
+ * PHP version 5
+ *
+ * Copyright (c) 2013, Mike Pretzlaw
+ * All rights reserved.
+ *
+ * @category   mage_devmode
+ * @package    Cms.php
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devmode/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devmode
+ * @since      $VERSION$
+ */
+
+/**
+ * Class Cms.
+ *
+ * @category   mage_devmode
+ * @author     Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright  2013 Mike Pretzlaw
+ * @license    http://github.com/sourcerer-mike/mage_devmode/blob/master/License.md BSD 3-Clause ("BSD New")
+ * @link       http://github.com/sourcerer-mike/mage_devmode
+ * @since      $VERSION$
+ */
+class LeMike_DevMode_Block_Toolbox_Catalog_Category extends LeMike_DevMode_Block_Toolbox
+{
+    protected $_template = 'lemike/devmode/toolbox/catalog/category.phtml';
+
+
+    public function getEditUrl()
+    {
+        return $this->getBackendUrl(
+            'adminhtml/catalog_category/index',
+            array(
+                 'id' => Mage::registry('current_category')->getId(),
+                'clear' => 1
+            )
+        );
+    }
+} 
